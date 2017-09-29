64576ab40c63c6b6ecaf1c8da6fe37e82923f719
shy <shyamal.punekar@gmail.com>
2017-09-29 09:51:59 -0700

    Add component to describe about me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { routing } from './app.routing';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,15 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
-
+import { AboutComponent } from './about/about.component';
 const appRoutes: Routes = [
   {
     path: '',
     component: WelcomeComponent
+  },
+  {
+    path: 'about-me',
+    component: AboutComponent
   }
 ];
 
